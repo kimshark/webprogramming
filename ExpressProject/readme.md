50a322805968d92340c0c5c8e6ebf796b5e1875e
kimshark <kimshark3747@gmail.com>
2026-05-08 18:41:51 +0900

    SUCCESS: Unity Build folder is finally included

--- ExpressProject/readme.md
+++
@@ -1,1 +0,0 @@
-Unity를 Express로 구동하는 프로젝트
--- react-website/src/components/Card.jsx
+++
@@ -1,9 +0,0 @@
-export default function Card({ title, description }) {
-  return (
-    <div className="card">
-      <h2 className="card-title">{title}</h2>
-      <p className="card-description">{description}</p>
-      <div className="card-glow"></div>
-    </div>
-  );
-}
